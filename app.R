8995356c35ace1550561ab57ccfc28e59d1cf928
Bill Capehart <William.Capehart@sdsmt.edu>
2021-10-07 16:10:14 -0600

    Update app.R

--- app.R
+++ app.R
@@ -157,10 +157,11 @@ ui = fluidPage(
             p("Using the map, select the state and then the numbered region in the state from the pull-down menus"),
             p("Next, you can select the period overwhich the budget is calculated with the sliders"),
             p("The results will be shown below"),
-            h3("Reference").
+            h3("Reference"),
             p("Thornthwaite, C.W.; Mather, J.R. The Water Balance; Laboratory in Climatology, Johns Hopkins University: Baltimore, MD, USA,
 1955; Volume 8, pp. 1–104."),
-            p("Emanuele Eccel's R ClimClass Package. https://CRAN.R-project.org/package=ClimClass")
+            p("Emanuele Eccel's R ClimClass Package. https://CRAN.R-project.org/package=ClimClass"),
+          
             h2("Budget Graph"), 
             plotOutput(outputId = "thornthwaitePlot"),
             
